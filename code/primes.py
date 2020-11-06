1e9efa838620c23fc6370546d97a6777b489ed3e
Michael S <33690140+michaelssavage@users.noreply.github.com>
2020-11-06 13:12:09 +0000

    Add files via upload

---
+++ code/primes.py
@@ -0,0 +1,13 @@
+def is_prime(number):
+#funtion goes here
+    for element in range(2,number):
+         if number % element == 0:
+              print(number)
+              print("NOT PRIME")
+              return False
+    print(number)
+    print("PRIME")
+    return True
+
+x=int(input("enter:"))
+is_prime(x)
